776bb746d41a3b83d8c84663e638e3864cf216d7
atime4ux <atime4ux@nate.com>
2020-04-16 09:54:56 +0900

    init commit

---
+++ webpageCaller/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace webpageCaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MainCallerState = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RetryCallerState = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(20, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Webpage Caller";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Main Caller -";
+            // 
+            // MainCallerState
+            // 
+            this.MainCallerState.AutoSize = true;
+            this.MainCallerState.Location = new System.Drawing.Point(104, 102);
+            this.MainCallerState.Name = "MainCallerState";
+            this.MainCallerState.Size = new System.Drawing.Size(38, 12);
+            this.MainCallerState.TabIndex = 3;
+            this.MainCallerState.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Retry Caller -";
+            // 
+            // RetryCallerState
+            // 
+            this.RetryCallerState.AutoSize = true;
+            this.RetryCallerState.Location = new System.Drawing.Point(105, 124);
+            this.RetryCallerState.Name = "RetryCallerState";
+            this.RetryCallerState.Size = new System.Drawing.Size(38, 12);
+            this.RetryCallerState.TabIndex = 5;
+            this.RetryCallerState.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 145);
+            this.Controls.Add(this.RetryCallerState);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MainCallerState);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Webpage Caller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label MainCallerState;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label RetryCallerState;
+    }
+}
+
---
+++ webpageCaller/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Messaging;
+using System.Windows.Forms;
+
+namespace webpageCaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        libCommon.clsUtil objUtil;
+
+        System.Threading.Thread mainCaller;
+        System.Threading.Thread retryCaller;
+
+        private Type[] objType;
+        private string qPath;
+        private string qPath_retry;//재시도 큐
+        private string encodeType;
+
+        private int sleep;
+        private int MaxCallingFailCnt;//실패 재시도 횟수
+        private int retryInterval;//재시도 함수 주기
+        private int rcvTimeOut;//큐 대기시간
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            objUtil = new libCommon.clsUtil();
+
+            this.objType = new Type[] { typeof(libMyUtil.pageCallingInfo) };
+            this.qPath = objUtil.getAppCfg("queuePath");
+            this.qPath_retry = objUtil.getAppCfg("queuePath_retry");
+            this.encodeType = objUtil.getAppCfg("encodeType").ToUpper();
+
+
+
+            this.sleep = objUtil.ToInt32(objUtil.getAppCfg("sleep")) * 1000;
+            if (this.sleep == 0)
+                this.sleep = 1000;
+
+            this.MaxCallingFailCnt = objUtil.ToInt32(objUtil.getAppCfg("MaxCallingFailCnt"));
+            if (this.MaxCallingFailCnt == 0)
+                this.MaxCallingFailCnt = 10;
+
+            this.rcvTimeOut = objUtil.ToInt32(objUtil.getAppCfg("rcvTimeout"));
+            if (this.rcvTimeOut == 0)
+                this.rcvTimeOut = 1;
+
+            this.retryInterval = objUtil.ToInt32(objUtil.getAppCfg("retryInterval")) * 1000;
+            if (this.retryInterval == 0)
+                this.retryInterval = 60 * 1000;
+
+
+
+            
+
+            Application.ApplicationExit += new EventHandler(Application_ApplicationExit);
+
+            MainCallerState.Text = "정지됨";
+            RetryCallerState.Text = "정지됨";
+
+            button1.PerformClick();
+        }
+        /// <summary>
+        /// 메인 함수
+        /// </summary>
+        private void RunCaller()
+        {
+            libMyUtil.clsMSMQ objMSMQ = new libMyUtil.clsMSMQ();
+
+            while (objMSMQ.canReadQueue(qPath))
+                prcRcvObject(objMSMQ.receiveData(this.qPath, this.objType, this.rcvTimeOut));
+
+            libMyUtil.clsThread.SetLabel(MainCallerState, "오류");
+            MessageBox.Show(string.Format("해당큐({0})에 접근할 수 없습니다.", qPath));
+        }
+        /// <summary>
+        /// 재시도 함수
+        /// </summary>
+        private void tryAgain()
+        {
+            libMyUtil.clsMSMQ objMSMQ = new libMyUtil.clsMSMQ();
+            System.Diagnostics.Stopwatch objWatch = new System.Diagnostics.Stopwatch();
+
+            int retryMsgCnt;
+            int i;
+
+            while (objMSMQ.canReadQueue(qPath_retry))
+            {
+                retryMsgCnt = objMSMQ.queueMsgCnt(this.qPath_retry);
+                for (i = 0; i < retryMsgCnt; i++)
+                {
+                    libMyUtil.pageCallingInfo rcvObj = (libMyUtil.pageCallingInfo)objMSMQ.peekData(this.qPath_retry, this.objType, this.rcvTimeOut);
+                    if ((System.DateTime.Now - rcvObj.lastTry).Duration().TotalMilliseconds < this.retryInterval)
+                        break;
+                    prcRcvObject(objMSMQ.receiveData(this.qPath_retry, this.objType, this.rcvTimeOut));
+                }
+
+                System.Threading.Thread.Sleep(this.retryInterval);
+            }
+
+            libMyUtil.clsThread.SetLabel(RetryCallerState, "오류");
+            MessageBox.Show(string.Format("해당큐({0})에 접근할 수 없습니다.", qPath_retry));
+        }
+        /// <summary>
+        /// 수신된 메시지 처리, null수신시 sleep
+        /// </summary>
+        private void prcRcvObject(object rcvObj)
+        {
+            if (rcvObj != null)
+            {
+                System.Diagnostics.Stopwatch objWatch = new System.Diagnostics.Stopwatch();
+                objWatch.Start();
+
+                libMyUtil.clsMSMQ objMSMQ = new libMyUtil.clsMSMQ();
+                libMyUtil.pageCallingInfo callingInfo = (libMyUtil.pageCallingInfo)rcvObj;
+
+                string Result;
+                string url = callingInfo.url;
+                string postData = callingInfo.postData;
+                int timeOut = callingInfo.timeOut;
+                
+                if (callingInfo.httpMethod.Equals("POST"))
+                    Result = libMyUtil.clsWeb.SendPostData(postData, url, this.encodeType, timeOut * 1000);
+                else if (callingInfo.httpMethod.Equals("GET"))
+                    Result = libMyUtil.clsWeb.SendQueryString(postData, url, timeOut * 1000);
+                else
+                    Result = "UNKNOWN HTTP METHOD";
+
+                objUtil.writeLog(string.Format("CALL RESULT : {0}\r\nFKEY:{1}\r\nURL:{2}", Result, callingInfo.FKEY, callingInfo.url));
+
+                if (Result.Equals(callingInfo.callresult))
+                    UpdateResult(callingInfo, Result);//성공 결과 DB에 저장
+                else if (Result.Equals("FAIL"))
+                {
+                    //재시도
+                    callingInfo.failCnt++;//실패 카운트 증가
+                    callingInfo.lastTry = System.DateTime.Now;//시간 기록
+                    if (callingInfo.failCnt < this.MaxCallingFailCnt)//설정한 횟수만큼 재시도
+                        objMSMQ.sendData(qPath_retry, callingInfo);//재시도 큐에 저장
+                    else
+                    {
+                        callingInfo.writeLog(Result);//최종 실패 메시지 로그 기록
+                        UpdateResult(callingInfo, Result);//실패 결과 DB에 저장
+                    }
+                }
+                else
+                {
+                    callingInfo.writeLog(Result);//최종 실패 메시지 로그 기록
+                    UpdateResult(callingInfo, Result);//실패 결과 DB에 저장
+                }
+
+                objWatch.Stop();
+                if (System.DateTime.Now.Millisecond > 500)
+                    objUtil.writeLog(string.Format("TIME SPAN : {0} millisecond", objWatch.Elapsed.TotalMilliseconds.ToString()));
+            }
+            else
+                System.Threading.Thread.Sleep(this.sleep);//큐에 메시지 없으면 슬립
+        }
+        /// <summary>
+        /// tb_aURLresult 테이블 결과 업데이트
+        /// </summary>
+        /// <param name="Result">전달받은 결과값</param>
+        private void UpdateResult(libMyUtil.pageCallingInfo callingInfo, string Result)
+        {
+            libCommon.clsDB objDB = new libCommon.clsDB();
+            System.Data.SqlClient.SqlConnection dbCon;
+            System.Data.SqlClient.SqlTransaction TRX;
+
+            string updateResult;
+
+            if (System.Text.Encoding.Default.GetByteCount(Result) > 1000)
+                Result = Result.Substring(0, 500);
+            
+            try
+            {
+                dbCon = objDB.GetConnection();
+                TRX = dbCon.BeginTransaction();
+                updateResult = libMyUtil.clsCmnDB.UPDATE_DB(dbCon, TRX, "tb_aURLresult", "RESULT", Result, "aURLidx|FKEY", callingInfo.aURLset_Idx + "|" + callingInfo.FKEY);
+                if (updateResult.Equals("FAIL"))
+                {
+                    TRX.Rollback();
+                    objUtil.writeLog(string.Format("FAIL UPDATE URL RESULT\r\nFKEY:{0}\r\nResult:{1}", callingInfo.FKEY, Result));
+                }
+                else
+                    TRX.Commit();
+                dbCon.Close();
+            }
+            catch (Exception ex)
+            {
+                objUtil.writeLog("ERR UPDATE URL CALL RESULT : " + ex.ToString());
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (mainCaller == null)
+            {
+                mainCaller = new System.Threading.Thread(new System.Threading.ThreadStart(RunCaller));
+                mainCaller.Name = "mainCaller";
+                mainCaller.Start();
+                MainCallerState.Text = "실행중";
+            }
+            else
+            {
+                mainCaller.Abort();
+                MainCallerState.Text = "정지됨";
+                mainCaller = null;
+            }
+
+            if (retryCaller == null)
+            {
+                retryCaller = new System.Threading.Thread(new System.Threading.ThreadStart(tryAgain));
+                retryCaller.Name = "retryCaller";
+                retryCaller.Start();
+                RetryCallerState.Text = "실행중";
+            }
+            else
+            {                
+                retryCaller.Abort();
+                RetryCallerState.Text = "정지됨";
+                retryCaller = null;
+            }
+        }
+        void Application_ApplicationExit(object sender, EventArgs e)
+        {
+            try
+            {
+                mainCaller.Abort();
+                retryCaller.Abort();
+            }
+            catch (Exception ex)
+            {
+                objUtil.writeLog("ERR ABORT THREAD : " + ex.ToString());
+            }
+        }
+
+
+
+        /// <summary>
+        /// 메시지 큐 감시
+        /// </summary>
+        private IAsyncResult watchMessageQueue(string queuePath, Type[] objectType)
+        {
+            MessageQueue myQ = new MessageQueue(queuePath);
+            
+            myQ.Formatter = new XmlMessageFormatter(objectType);
+            myQ.ReceiveCompleted += new ReceiveCompletedEventHandler(myQ_ReceiveCompleted);
+            
+            return myQ.BeginReceive();
+        }
+        /// <summary>
+        /// 메시지 수신 이벤트 발생시 실행
+        /// </summary>
+        void myQ_ReceiveCompleted(object sender, ReceiveCompletedEventArgs e)
+        {
+            MessageQueue myQ = (MessageQueue)sender;
+            object rcvObj = myQ.EndReceive(e.AsyncResult).Body;
+            
+            prcRcvObject(rcvObj);
+            watchMessageQueue(qPath, objType);
+        }
+    }
+}
